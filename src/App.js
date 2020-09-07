df6b17821c18646215d61572a7afd9bf71b82ad2
rywake <60336812+rywake@users.noreply.github.com>
2020-09-07 11:19:42 -0700

    Created intro page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="introHeader">Linear vs. Logistic Regression</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Digression on Regression!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
