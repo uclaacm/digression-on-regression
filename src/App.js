31949ef16beb59a7704d44c9dec33249f86b95e2
rywake <60336812+rywake@users.noreply.github.com>
2020-09-07 15:34:45 -0700

    Created introductory page, added packaged.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import './App.css';
+import {faChartLine} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Button from '@material-ui/core/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
+    <div style={{backgroundColor: '#FBFBFB'}}>
+      <header>
         <h1 className="introHeader">Linear vs. Logistic Regression</h1>
-        <p>
+      </header>
+      <div>
+        <p className="App-header">
           Digression on Regression!
         </p>
-      </header>
+        <Container>
+          <Row>
+            <Col xs={12} md={8}><p id="intro">An Introduction to Linear and Logistic Regression. </p> </Col>
+            <Col xs={6} md={4}><FontAwesomeIcon id="chart-icon" icon={faChartLine}/></Col>
+          </Row>
+        </Container>
+
+        <div style={{textAlign: 'center', marginTop: "2vmin"}}>
+          <Button variant="contained"
+            style={{
+              backgroundColor: "#95CF67",
+              color: "white",
+              fontSize: "3vmin"
+            }}
+          >
+            Start
+          </Button>
+        </div>
+
+      </div>
     </div>
   );
 }
