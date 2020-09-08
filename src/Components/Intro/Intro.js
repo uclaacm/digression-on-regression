54ea8b31c25bafcc5d3edaaf05421eb70f7641e4
rywake <60336812+rywake@users.noreply.github.com>
2020-09-08 01:27:19 -0700

    Removed bootstrap and replaces with material UI API's.

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -3,10 +3,7 @@ import './Intro.css';
 import {faChartLine} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Button from '@material-ui/core/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Grid from '@material-ui/core/Grid';
 
 function Intro(props) {
     return (
@@ -15,23 +12,27 @@ function Intro(props) {
             <h1 className="introHeader">Linear vs. Logistic Regression</h1>
           </header>
           <div>
-            <p className="App-header">
+            <p id="Digression">
               Digression on Regression
             </p>
-            <Container>
-              <Row>
-                <Col xs={12} md={8}><p id="intro">
-                  What is linear and logistic regression? 
-                  Why does it matter? This lesson will give you an introduction to 
-                  linear and logistic regression and explain why it is such an important topic. 
+
+            <div>
+              <Grid container spacing={2}>
+                <Grid item xs={7}>
+                  <p id="intro">
+                    What is linear and logistic regression? 
+                    Why does it matter? This lesson will give you an introduction to 
+                    linear and logistic regression and explain why it is such an important topic. 
                   </p> 
-                </Col>
-                <Col xs={6} md={4}><FontAwesomeIcon id="chart-icon" icon={faChartLine}/>
-                </Col>
-    
-              </Row>
-            </Container>
-    
+                </Grid>
+                <Grid item xs={3}>
+                  <FontAwesomeIcon id="chart-icon" icon={faChartLine}/>
+                </Grid>
+              </Grid>
+            </div>
+
+
+
             <div style={{textAlign: 'center', marginTop: "1em", marginBottom: "2em"}}>
               <Button variant="contained"
                 style={{
