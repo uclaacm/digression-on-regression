c9570fff6fe7920c56f6b2981011f1e307054433
rywake <60336812+rywake@users.noreply.github.com>
2020-09-07 23:46:12 -0700

    Moved code in App.js to seperate folder.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
 import React from 'react';
 import './App.css';
-import {faChartLine} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import Button from '@material-ui/core/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Intro from './Components/Intro/Intro.js';
 
 function App() {
   return (
-    <div style={{backgroundColor: '#FBFBFB'}}>
-      <header>
-        <h1 className="introHeader">Linear vs. Logistic Regression</h1>
-      </header>
-      <div>
-        <p className="App-header">
-          Digression on Regression!
-        </p>
-        <Container>
-          <Row>
-            <Col xs={12} md={8}><p id="intro">What is linear and logistic regression? 
-            Why does it even matter? This lesson will give you an introduction to 
-            linear and logistic regression and explain why it is such an important topic. </p> </Col>
-            <Col xs={6} md={4}><FontAwesomeIcon id="chart-icon" icon={faChartLine}/></Col>
-          </Row>
-        </Container>
-
-        <div style={{textAlign: 'center', marginTop: "2vmin"}}>
-          <Button variant="contained"
-            style={{
-              backgroundColor: "#95CF67",
-              color: "white",
-              fontSize: "3vmin"
-            }}
-          >
-            Start
-          </Button>
-        </div>
-
-      </div>
-    </div>
+    Intro()
   );
 }
 
--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './Intro.css';
 import {faChartLine} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Button from '@material-ui/core/Button';
@@ -10,22 +10,25 @@ import Col from 'react-bootstrap/Col';
 
 function Intro(props) {
     return (
-        <div style={{backgroundColor: '#FBFBFB'}}>
+        <div style={{backgroundColor: '#FFFFFF'}}>
           <header>
             <h1 className="introHeader">Linear vs. Logistic Regression</h1>
           </header>
           <div>
             <p className="App-header">
-              Digression on Regression!
+              Digression on Regression
             </p>
             <Container>
               <Row>
-                <Col xs={12} md={8}><p id="intro">What is linear and logistic regression? 
-                Why does it even matter? This lesson will give you an introduction to 
-                linear and logistic regression and explain why it is such an important topic. </p> </Col>
-                <div id="Show">
-                    <Col xs={6} md={4}><FontAwesomeIcon id="chart-icon" icon={faChartLine}/></Col>
-                </div>
+                <Col xs={12} md={8}><p id="intro">
+                  What is linear and logistic regression? 
+                  Why does it matter? This lesson will give you an introduction to 
+                  linear and logistic regression and explain why it is such an important topic. 
+                  </p> 
+                </Col>
+                <Col xs={6} md={4}><FontAwesomeIcon id="chart-icon" icon={faChartLine}/>
+                </Col>
+    
               </Row>
             </Container>
     
